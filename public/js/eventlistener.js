089e67453dac3e6849426362357ab6b6550ec24f
mia23ali <izzuddin.binali@gmail.com>
2021-06-14 11:15:12 +0800

    remove back to top

--- public/js/eventlistener.js
+++ public/js/eventlistener.js
@@ -1,26 +1,3 @@
-//back to top function
-
-/*
-
-let topButton = document.getElementById("backToTop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction(){
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        topButton.style.display = "block";
-    }
-    else{
-        topButton.style.display = "none";
-    }
-}
-
-function topFunction(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-*/
-
 //index nav
 
 let indexNav = document.getElementById("indexNav");
