46833fe8e00a82cd667b3479770555e5d90f8a04
mia23ali <izzuddin.binali@gmail.com>
2021-06-15 00:04:38 +0800

    Co-authored-by: Wafiy Damit <wafiyphd@users.noreply.github.com>

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://admin:orNwVUCkRu7uNIUa@embn.0oom9.mongodb.net/EMBN?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://Mia:Pe2mCF8laITxK0j0@emforum1.9g25b.mongodb.net/EMBN?retryWrites=true&w=majority"
     };
